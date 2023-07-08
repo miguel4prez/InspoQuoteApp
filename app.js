fa728f0164159da5fdeb805d4ed732dcfa8f3d87
Miguel Vasquez <79591626+miguel4prez@users.noreply.github.com>
2023-07-08 09:48:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const url = 'https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&c
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '5f845b1859mshf77be2fc59967b7p14cf1ejsn76fcf7dc741d',
+    'X-RapidAPI-Key': '',
     'X-RapidAPI-Host': 'andruxnet-random-famous-quotes.p.rapidapi.com'
   }
 };
